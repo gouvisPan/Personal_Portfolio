c779c343f23a3a4e38e1de39c2ace81ac0c6cc9b
Panagiotis Gouvis <97959624+gouvisPan@users.noreply.github.com>
2022-07-13 06:02:55 +0300

    AddingProjects

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -3,6 +3,7 @@ import { FaUniversity } from "react-icons/fa";
 import { FaFolderOpen } from "react-icons/fa";
 import { AiFillStar } from "react-icons/ai";
 import "./About.css";
+import ME from "./../../assets/man.jpg";
 
 const About = () => {
   return (
@@ -10,39 +11,44 @@ const About = () => {
       <h4>Some info about me</h4>
 
       <div className="container about_container">
-        <div className="me_img"></div>
-        <div className="info">
-          <article className="about_card">
-            <FaUniversity />
-            <h5>Education</h5>
-            <small>
-              <strong>Excellence </strong> at Applied Informatics UoM
-            </small>
-          </article>
-          <article className="about_card">
-            <AiFillStar />
-            <h5>Skillset</h5>
-            <small>Android and Web development</small>
-          </article>
-          <article href="#projects" className="about_card">
-            <FaFolderOpen />
-            <h5>Projects</h5>
-            <small>
-              10+ <strong>Kotlin</strong> and
-              <strong> React.js</strong> projects
-            </small>
-          </article>
+        <div className="me_con">
+          <img src={ME} className="me_img" />
         </div>
-        <div className="info_descr">
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
-          </p>
+
+        <div className="info">
+          <div className="cards_container">
+            <article className="about_card">
+              <FaUniversity />
+              <h5>Education</h5>
+              <small>
+                <strong>Excellence </strong> at Applied Informatics UoM
+              </small>
+            </article>
+            <article className="about_card">
+              <AiFillStar />
+              <h5>Skillset</h5>
+              <small>Android and Web development</small>
+            </article>
+            <article href="#projects" className="about_card">
+              <FaFolderOpen />
+              <h5>Projects</h5>
+              <small>
+                10+ <strong>Kotlin</strong> and
+                <strong> React.js</strong> projects
+              </small>
+            </article>
+          </div>
+          <div className="info_descr">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+          </div>
         </div>
       </div>
     </section>
--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -1,7 +1,28 @@
 import React from "react";
+import "./Projects.css";
+import WeatherApp from "./../../assets/FullApp.png";
+import Dummy from "./../../assets/dummy.png";
 
 const Projects = () => {
-  return <section id="projects">Projects</section>;
+  return (
+    <section id="projects">
+      <h4>A glance into my portfolio</h4>
+      <div className="protfolio_container">
+        <div className="react_projects_container">
+          <div className="project_card">
+            <img src={WeatherApp} />
+            <h5>Weather App</h5>
+          </div>
+          <div className="project_card">
+            <img src={Dummy} />
+            <h5>Dummy App</h5>
+          </div>
+        </div>
+
+        <div className="android_projects_container"></div>
+      </div>
+    </section>
+  );
 };
 
 export default Projects;
